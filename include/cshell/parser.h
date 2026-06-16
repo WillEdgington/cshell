5114014ec1088c78f66b1e7ffd47278ef8fe0d60
WillEdgington <willedge037@gmail.com>
2026-06-16 18:07:55 +0100

    feat: (parser) multi-command parsing compatiblity and add stderr logging

--- include/cshell/parser.h
+++ include/cshell/parser.h
@@ -3,14 +3,25 @@
 
 #define MAX_ARGS 64
 
-typedef struct {
+#include "clib/arena.h"
+
+typedef struct Command {
   char *input_redirect;
   char *output_redirect;
-  int arg_count;
-  int is_background;
+  struct Command *next;
   char *args[MAX_ARGS + 1];
+  int arg_count;
 } Command;
 
-int cshell_parse_line(char *line, Command *cmd);
+typedef struct {
+  Command *head;
+  Command *tail;
+  Arena *arena;
+  int is_background;
+  int command_count;
+} Pipeline;
+
+void pipeline_init(Pipeline *pipe, Arena *a);
+int cshell_parse_line(char *line, Pipeline *pipe);
 
 #endif
