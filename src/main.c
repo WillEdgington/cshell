9395cb59b9a72134ea4623f8a2ae49fb9348745f
WillEdgington <willedge037@gmail.com>
2026-06-17 14:34:49 +0100

    feat: (main) implement new multi-command parse and execution compatibility in main runtime loop

--- src/main.c
+++ src/main.c
@@ -3,14 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "clib/arena.h"
 #include "cshell/executor.h"
 #include "cshell/parser.h"
 
 #define MAX_LINE_LENGTH 4096
+#define PIPELINE_ARENA_SLAB_SIZE 8192 // 8 KB
 
 static void handle_shutdown(int sig) {
   (void)sig;
+  printf("\n");
   exit(1);
+}
+
+static void handle_exit(int sig, Arena *a) {
+  arena_free(a);
+  handle_shutdown(sig);
 }
 
 int main(void) {
@@ -18,23 +26,26 @@ int main(void) {
   signal(SIGTERM, handle_shutdown);
 
   char line[MAX_LINE_LENGTH];
-  Command cmd;
-  memset(&cmd, 0, sizeof(Command));
+  Pipeline pipe;
+  Arena arena;
 
+  arena_init(&arena, PIPELINE_ARENA_SLAB_SIZE);
   while (1) {
+    arena_reset(&arena);
+    pipeline_init(&pipe, &arena);
+
     printf("cshell> ");
     fflush(stdout);
 
-    if (fgets(line, sizeof(line), stdin) == NULL) {
-      printf("\n");
-      handle_shutdown(SIGTERM);
-    }
+    if (fgets(line, sizeof(line), stdin) == NULL)
+      handle_exit(SIGTERM, &arena);
 
-    if (cshell_parse_line(line, &cmd) == -1)
+    if (cshell_parse_line(line, &pipe) == -1)
       continue;
 
-    if (cshell_execute(&cmd) == SHELL_STATUS_EXIT)
-      handle_shutdown(SIGTERM);
+    if (cshell_execute_pipeline(&pipe) == SHELL_STATUS_EXIT)
+      handle_exit(SIGTERM, &arena);
   }
+  arena_free(&arena);
   return 0;
 }
