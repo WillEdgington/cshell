79fc683b89ec12907b9b35bcd3b937eeb4490ad2
WillEdgington <willedge037@gmail.com>
2026-06-30 12:45:28 +0100

    feat: (main) integrate terminal state toggling and history navigation driver

--- src/main.c
+++ src/main.c
@@ -6,11 +6,14 @@
 
 #include "clib/arena.h"
 #include "cshell/executor.h"
+#include "cshell/handler.h"
+#include "cshell/history.h"
+#include "cshell/linereader.h"
 #include "cshell/parser.h"
 #include "cshell/prompt.h"
-#include "cshell/tracker.h"
 #include "cshell/runtime.h"
-#include "cshell/handler.h"
+#include "cshell/terminal.h"
+#include "cshell/tracker.h"
 
 #define MAX_LINE_LENGTH 4096
 #define PIPELINE_ARENA_SLAB_SIZE 8192 // 8 KB
@@ -38,6 +41,8 @@ int main(void) {
   cshell_init_signals();
   cshell_tracker_init();
 
+  cshell_terminal_init();
+  cshell_history_init();
   shell_r.last_exit_status = 0;
 
   while (1) {
@@ -47,20 +52,26 @@ int main(void) {
 
     cshell_display_prompt();
 
-    if (fgets(line, sizeof(line), stdin) == NULL) {
-      if (errno != EINTR) {
+    cshell_terminal_mode_raw();
+    ssize_t read_bytes = cshell_read_line(line, MAX_LINE_LENGTH);
+    cshell_terminal_mode_canonical();
+
+    if (read_bytes < 0) {
+      if (errno != EINTR)
         handle_exit(SIGTERM, &arena);
-      }
+
       shell_r.last_exit_status = errno;
       errno = 0;
       continue;
     }
 
+    cshell_history_add(line);
+
     if (cshell_parse_line(line, &pipe) == -1) {
       shell_r.last_exit_status = 1;
       continue;
     }
-    
+
     cshell_handle_execution(&pipe);
     if (shell_r.last_exit_status == SHELL_STATUS_EXIT)
       handle_exit(SIGTERM, &arena);
