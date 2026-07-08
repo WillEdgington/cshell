62666f0747d62c863d50578e497344b129dd249f
WillEdgington <willedge037@gmail.com>
2026-07-08 18:28:58 +0100

    test: (completion) add test coverage to validate behaviour of new list matches of a prefix method

--- tests/test_completion.c
+++ tests/test_completion.c
@@ -76,12 +76,50 @@ static void test_completion_ambiguity_or_none(void) {
   teardown_mock_file(filename_2);
 }
 
+static void test_completion_list_collection(void) {
+  char *matches[MAX_COMPLETION_LIST_LEN] = {0};
+  char *f1 = "_test_batch_1_49354322";
+  char *f2 = "_test_batch_2_43425666";
+  char *f3 = "_test_batch_3_53243663";
+
+  setup_mock_file(f1);
+  setup_mock_file(f2);
+  setup_mock_file(f3);
+
+  int total_found = cshell_completion_list_matches("_test_batch_", 0, matches);
+
+  teardown_mock_file(f1);
+  teardown_mock_file(f2);
+  teardown_mock_file(f3);
+
+  ASSERT_INT_EQ(
+      total_found, 3,
+      "Scanner array generation must catch all matching system paths");
+  ASSERT_PTR_NOT_NULL(
+      matches[0], "First collection slot must contain a valid string pointer");
+
+  int space_terms = 0, collected = 0;
+  for (int i = 0; i < total_found && i < MAX_COMPLETION_LIST_LEN; i++) {
+    if (matches[i] != NULL) {
+      if (strstr(matches[i], "_test_batch_") != NULL) {
+        space_terms += matches[i][strlen(matches[i]) - 1] == ' ' ? 1 : 0;
+        collected++;
+      }
+      free(matches[i]);
+    }
+  }
+
+  ASSERT_INT_EQ(space_terms, collected,
+                "Collected match files should terminate with space indicators");
+}
+
 int main(void) {
   printf("\nRunning: %s\n", __FILE__);
 
   test_completion_local_file();
   test_completion_nested_directory();
   test_completion_ambiguity_or_none();
+  test_completion_list_collection();
 
   test_summary();
   return tests_failed > 0 ? 1 : 0;
